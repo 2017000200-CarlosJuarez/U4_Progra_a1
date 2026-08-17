967637696c7be1b9962687d16c8f38b244a93f30
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-08-17 12:14:46 -0600

    Add files via upload

---
+++ Ejercicio_en_Clase/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Ejercicio_en_Clase
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            numericUpDown1 = new NumericUpDown();
+            label3 = new Label();
+            label4 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label5 = new Label();
+            comboBox1 = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(66, 109);
+            label1.Name = "label1";
+            label1.Size = new Size(107, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Nombre Completo";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(112, 172);
+            label2.Name = "label2";
+            label2.Size = new Size(33, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Edad";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(209, 109);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(213, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(211, 164);
+            numericUpDown1.Maximum = new decimal(new int[] { 20, 0, 0, 0 });
+            numericUpDown1.Minimum = new decimal(new int[] { 3, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(213, 23);
+            numericUpDown1.TabIndex = 3;
+            numericUpDown1.Value = new decimal(new int[] { 3, 0, 0, 0 });
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(472, 170);
+            label3.Name = "label3";
+            label3.Size = new Size(119, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Fecha de Nacimiento";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(266, 31);
+            label4.Name = "label4";
+            label4.Size = new Size(317, 15);
+            label4.TabIndex = 5;
+            label4.Text = "Formulario de estudiante del Colegio Salesiano Don Bosco ";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(615, 164);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(213, 23);
+            dateTimePicker1.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(78, 222);
+            label5.Name = "label5";
+            label5.Size = new Size(97, 15);
+            label5.TabIndex = 7;
+            label5.Text = "Nivel Academico";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Nursery", "Pre-Primaria", "Primaria", "Secundaria", "Diversificado" });
+            comboBox1.Location = new Point(211, 219);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(213, 23);
+            comboBox1.TabIndex = 8;
+            comboBox1.Text = "Click aqui ===>";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(895, 575);
+            Controls.Add(comboBox1);
+            Controls.Add(label5);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(numericUpDown1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private NumericUpDown numericUpDown1;
+        private Label label3;
+        private Label label4;
+        private DateTimePicker dateTimePicker1;
+        private Label label5;
+        private ComboBox comboBox1;
+    }
+}
